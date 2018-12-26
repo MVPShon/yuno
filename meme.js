5cc6dfea0ff4e5aa0b8ec5e556d6f9660b3026a2
MVPShon <36894232+MVPShon@users.noreply.github.com>
2018-12-26 01:45:29 -0500

    Delete meme.js

--- meme.js
+++
@@ -1,8 +0,0 @@
-
-const Discord = require("discord.js");
-exports.run = async (yuno, message, args) => {
-    var redditmeme = require('reddit-memes');
-redditmeme(function(data, err) {
-    message.channel.send(`**${data.title[0]}** -- r/${data.subreddit[0]}\n${data.url[0]}`);
-});
-}
