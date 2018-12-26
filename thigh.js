e24f448e6f121150c84e119b1f6480fc2273a457
MVPShon <36894232+MVPShon@users.noreply.github.com>
2018-12-26 01:47:03 -0500

    Delete thigh.js

--- thigh.js
+++
@@ -1,9 +0,0 @@
-const Discord = require("discord.js");
-exports.run = async (yuno, message, args) => {
-if (!message.channel.nsfw) return message.reply("🔞 This command can only be used on an NSFW Channel! 🔞")
-var thighpic = require('redditthighs');
-thighpic(function(data, err) {
-    var embed = new Discord.RichEmbed()
-message.channel.send(`**${data.title[0]}** -- r/${data.subreddit[0]}\n${data.url[0]}`);
-});
-}
