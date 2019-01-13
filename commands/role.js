8cadea3e41209c1141e4147830ab554a7c8508fd
MVPShon <36894232+MVPShon@users.noreply.github.com>
2019-01-13 14:33:46 -0500

    Update role.js

--- commands/role.js
+++ commands/role.js
@@ -1,12 +1,21 @@
 const Discord = require("discord.js");
 exports.run = async (yuno, message, args) => {
+let colors = ["Red", "Blue", "Black", "Purple", "Orange", "Green", "Pink"];
+
+if(colors.indexOf(args[0]) > "-1") {
+
+//If it is one of those colors 
+
     let role = message.guild.roles.find(r => r.name == args[0]);
-    if (message.channel !== yuno.channels.get("526448416285261838")) return message.channel.send(`This command can only be used in ${yuno.channels.get("526448416285261838")}.`)
-    if (message.member.roles.has(role.id)) {
-        return message.member.removeRole(role.id, message.channel.send("Role Removed."))
-    }
-    if(args[0] !== "Red" || "Blue" || "Black" || "Purple" || "Orange" || "Green" || "Pink") return message.reply("Please choose from the valid list of roles. ");    
-    if (!role) return message.reply("Please choose from the valid list of roles. ")
+    if (message.member.roles.has(role.id)) return message.member.removeRole(role.id, message.channel.send("Role Removed."));
+    
     message.member.addRole(role)
     message.channel.send("Role Added.")
+
+} else {
+
+//If it isn't one of those colors
+return message.reply("Please choose from the valid list of roles. ");
+
+}
 }
